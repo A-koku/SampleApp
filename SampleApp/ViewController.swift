a7ca49496a3a41bf11765907e08a6bcb56e9eafa
深津裕樹 <fkt.x.day1993@gmail.com>
2018-03-27 16:37:43 +0900

    add Firebase Database

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -7,30 +7,52 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    var ref: DatabaseReference!
+
+    var messages: [String] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        ref = Database.database().reference().child("posts")
+
+        ref.observe(.childAdded, with: { snapshot in
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            print(snapshot)
+            guard let snapShotValue = snapshot.value as? [String: Any] else { return }
+            print(snapShotValue)
+            guard let title = snapShotValue["title"] as? String else { return }
+            print(title)
+            self.messages = self.messages + [title]
+        })
+
+
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return messages.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
-        cell.bindData(text: "section: \(indexPath.section) index: \(indexPath.row)")
+        cell.bindData(text: messages[indexPath.row])
         return cell
     }
 
